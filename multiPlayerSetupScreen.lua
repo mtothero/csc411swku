7cb5bf9396a65c73d3228e021770db6ef60b1322
Matt Tothero <mtothero26@gmail.com>
2015-03-28 12:44:40 -0400

    AutoLan addon.

--- multiPlayerSetupScreen.lua
+++ multiPlayerSetupScreen.lua
@@ -3,10 +3,11 @@ local scene = storyboard.newScene()
 local widget = require( "widget" )
 local multiHandler = require( "multiHandler" )
 
-local backButton, background, settingsButton, onePlayerButton, twoPlayerButton, highScoreButton, highScoresText, textGroup
-local createButton, findButton, tableView, roomInputField, roomInput
+local backButton, background, settingsButton, onePlayerButton, twoPlayerButton, highScoreButton
+local createButton, findButton, tableView
 local customFont
-local roomName, roomIP, roomCounter
+local roomName, roomIP
+local client, server, isClient, isServer, myPlayerID, playerDropped, clientDropped, numPlayers, clients, numberOfServers
 
 
 -- Called when the scene's view does not exist:
@@ -31,7 +32,9 @@ function scene:createScene( event )
         return false
     end
 
-    roomCounter = 1
+    numPlayers = 0
+    clients = {}
+    numberOfServers = 0
 end
 
 
@@ -42,14 +45,7 @@ function scene:enterScene( event )
     -- Create Button
     local function handleCreatePress( event )
        if ( "ended" == event.phase ) then
-            tableView:insertRow
-            {
-                rowColor = { default = { 255, 255, 255, 0 }},
-                lineColor = { 1, 1, 1 },
-                rowHeight = 100,
-                params = { input = roomIP, roomID = "GameRoom" .. roomCounter },
-            }
-            roomCounter = roomCounter+1
+            makeServer()
             createButton:setEnabled( false )
         end
     end
@@ -74,6 +70,7 @@ function scene:enterScene( event )
                 rowHeight = 100,
                 params = { input = roomInput, roomID = "Find Button" },
             }
+            makeClient()
         end
     end
     findButton = widget.newButton
@@ -105,7 +102,11 @@ function scene:enterScene( event )
         ip:setFillColor( 1 )
         ip.x = 650 
         ip.y = row.height * 0.5
-       
+    end
+
+    local function onRowTouch( event )
+        local row = event.row
+        print(row.params.roomID)
     end
 
     local options =
@@ -116,32 +117,9 @@ function scene:enterScene( event )
         height = 400,
         backgroundColor = { 1,1,1,.5},
         onRowRender = onRowRender,
+        onRowTouch = onRowTouch,
     }
     tableView = widget.newTableView( options )
-
-    local function textListener( event )
-        if ( event.phase == "began" ) then
-            -- begins editing
-        elseif ( event.phase == "ended" or event.phase == "submitted" ) then
-            roomInput = event.target.text 
-            native.setKeyboardFocus( nil )
-        elseif ( event.phase == "editing" ) then
-           -- is editing
-        end
-    end
-
-    -- Create text field
-    local fontSize = 14
-    local textToMeasure = display.newText("", (display.contentWidth / 2), 100, native.systemFont, fontSize)
-    local textHeight = textToMeasure.contentHeight
-    textHeight = textHeight+90
-
-
-    roomInputField = native.newTextField(display.contentWidth/2, display.contentHeight*(2/3), 300, textHeight )
-    roomInputField.font = native.newFont( native.systemFont, fontSize)
-    roomInputField.size = fontSize
-    roomInputField:addEventListener( "userInput", textListener )
-
 
     -- Settings button event listener, dummy button, blur effect
     local function handleSettingsPress( event )
@@ -206,8 +184,6 @@ function scene:enterScene( event )
     -- Function to handle back button
     local function handleBackPress( event )
         if ( "ended" == event.phase ) then
-            roomInputField:removeSelf( )
-            roomInputField = nil
             tableView:removeSelf( )
             tableView = nil
             createButton:removeSelf( )
@@ -271,6 +247,55 @@ end
 function scene:destroyScene( event )
 	local group = self.view
 end
+
+function makeServer()
+    if(isClient) then --if we were a client before, we need to unregister all the event listeners
+        isClient = false
+    end
+
+    server = require("Server") 
+    server:setCustomBroadcast("1 Player") 
+    server:start()
+    isServer = true
+
+    --add event listeners
+    Runtime:addEventListener("autolanPlayerJoined", addPlayer)
+end
+
+addPlayer = function(event)
+    local client = event.client --this is the client object, used to send messages
+    print("player joined",client)
+    --look for a client slot
+    numPlayers = numPlayers+1
+    clients[numPlayers] = client
+    client:sendPriority({1,numPlayers}) --initialization packet
+    client:sendPriority(getFullGameState()) --initialization packet 
+    server:setCustomBroadcast(numPlayers.." Players")
+end
+
+function makeClient()
+    if(isServer) then --if we were a server before, we need to unregister all the event listeners
+        isServer = false
+    end
+    print("making client")
+    client = require("Client")
+    client:start()
+    client:scanServers()
+    isClient = true
+end
+
+function createListItem(event) --displays found servers
+    tableView:insertRow
+    {
+        rowColor = { default = { 255, 255, 255, 0 }},
+        lineColor = { 1, 1, 1 },
+        rowHeight = 100,
+        params = { input = event.serverName, roomID = event.customBroadcast },
+    }
+    numberOfServers = numberOfServers+1
+end
+Runtime:addEventListener("autolanServerFound", createListItem)
+
 
 scene:addEventListener( "createScene", scene )
 
